4264a5592914279278bfb880b1a801a54ea9f951
Martin Absmeier <martin.absmeier@gmail.com>
2022-04-07 18:24:22 +0200

    Fix copyright and license

--- src/test/java/de/marabs/common/shell/TokenTest.java
+++ src/test/java/de/marabs/common/shell/TokenTest.java
@@ -1,6 +1,17 @@
 /*
- * This file is part of the Cliche project, licensed under MIT License.
- * See LICENSE.txt file in root folder of Cliche sources.
+ * Copyright 2022 Martin Absmeier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package de.marabs.common.shell;
 
@@ -10,9 +21,6 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * @author ASG
- */
 public class TokenTest {
 
     /**
